ceed8210a5ff0508bc046df24beceaa7f2a2bb58
m4thod0x <max.matweew@gmail.com>
2021-11-27 15:18:05 +0300

    Change test models User Task with FactoryBot

---
+++ test/factories/sequence.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  sequence :string, aliases: [:first_name, :last_name, :password, :avatar, :name, :state, :description] do |n|
+    "string#{n}"
+  end
+
+  sequence :email do |n|
+    "person#{n}@example.com"
+  end
+
+  sequence (:type) { ['Developer', 'Manager', 'Admin'].sample }
+
+  sequence (:expired_at) { Time.now }
+end
