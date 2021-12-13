45b0f08061af867071c9b3734dc67d0c06d6242b
m4thod0x <max.matweew@gmail.com>
2021-12-13 12:30:35 +0300

    Fixed model Task

--- test/factories/tasks.rb
+++ test/factories/tasks.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :task do
     name { generate :string }
     description { generate :string }
-    author_id { create :manager }
+    author_id { create :user }
     assignee_id { create :user }
   end
 end
