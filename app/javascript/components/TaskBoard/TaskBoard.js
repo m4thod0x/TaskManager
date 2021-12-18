faecfdd78c337bbd412e095422d8e4cc3d8b9b29
m4thod0x <max.matweew@gmail.com>
2021-12-18 07:09:48 +0300

    Change action show in Board contriller. Fix install webpacker-react

--- app/javascript/components/TaskBoard/TaskBoard.js
+++ app/javascript/components/TaskBoard/TaskBoard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Board from '@asseinfo/react-kanban';
+import '@asseinfo/react-kanban/dist/styles.css';
 
 const data = {
   columns: [
@@ -10,9 +11,9 @@ const data = {
         {
           id: 1,
           title: 'Add card',
-          description: 'Add capability to add a card in a column'
+          description: 'Add capability to add a card in a column',
         },
-      ]
+      ],
     },
     {
       id: 2,
@@ -21,12 +22,12 @@ const data = {
         {
           id: 2,
           title: 'Drag-n-drop support',
-          description: 'Move a card between the columns'
+          description: 'Move a card between the columns',
         },
-      ]
-    }
-  ]
-}
+      ],
+    },
+  ],
+};
 
 const TaskBoard = () => {
   return <Board initialBoard={data} />;
